337da703d9db66563a9ef8ad4ad76fe8bf5a0a7f
mothamatildo <mothamatildo@gmail.com>
2026-02-23 06:23:37 -0500

    Avance hasta el video 6 funcionando

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// RUTAS PARA EL ADMIN 
+
+Route::get('/admin', [App\Http\Controllers\AdminController::class, 'index'])
+->name('admin.index')
+->middleware ('auth');
