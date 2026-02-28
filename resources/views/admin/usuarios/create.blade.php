899249ce672f08581219416e4414c26f10be6ed5
mothamatildo <mothamatildo@gmail.com>
2026-02-28 06:07:33 -0500

    Primer commit sistema reserva de citas - modulo usuarios

--- resources/views/admin/usuarios/create.blade.php
+++ resources/views/admin/usuarios/create.blade.php
@@ -1,36 +1,72 @@
 @extends('layouts.admin')
-@section('content')
-<div class="row">
 
-    <h1>Registro de un nuevo usuario</h1>
+@section('content')
+
+<div class="row">
+    <div class="col-md-12">
+        <h1>Registro de un nuevo usuario</h1>
+    </div>
 </div>
 
 <hr>
 
 <div class="row">
     <div class="col-md-6">
-            <div class="card card-outline card-primary">
-              <div class="card-header">
-                <h3 class="card-title">Complete los datos</h3>
 
-                <div class="card-tools">
-                  <button type="button" class="btn btn-tool" data-card-widget="collapse">
-                    <i class="fas fa-minus"></i>
-                  </button>
-                </div>
-                <!-- /.card-tools -->
-              </div>
-              <!-- /.card-header -->
-              <div class="card-body">
-               <div class="row">
-                
-               </div>
-              </div>
-              <!-- /.card-body -->
+        <div class="card card-outline card-primary">
+
+            <div class="card-header">
+                <h3 class="card-title">Complete los datos</h3>
             </div>
-            <!-- /.card -->
-          </div>  
+
+            <div class="card-body">
+
+                <form action="#" method="POST">
+                    @csrf
+
+                    <!-- Nombre -->
+                    <div class="form-group">
+                        <label>Nombre del usuario</label> <b>*</b>
+                        <input type="text" class="form-control" name="name">
+                    </div>
+
+                    <!-- Email -->
+                    <div class="form-group">
+                        <label>Email</label><b>*</b>
+                        <input type="email" class="form-control" name="email">
+                    </div>
+
+                    <!-- Password -->
+                    <div class="form-group">
+                        <label>Password</label><b>*</b>
+                        <input type="password" class="form-control" name="password">
+                    </div>
+
+                    <!-- Confirmación -->
+                    <div class="form-group">
+                        <label>Password verificación</label><b>*</b>
+                        <input type="password" class="form-control" name="password_confirmation">
+                    </div>
+
+                    <hr>
+
+                    <div class="form-group">
+                        <a href="{{ url('admin/usuarios') }}" class="btn btn-secondary">
+                            Cancelar
+                        </a>
+
+                        <button type="submit" class="btn btn-primary">
+                            Registrar usuario
+                        </button>
+                    </div>
+
+                </form>
+
+            </div>
+
+        </div>
+
     </div>
 </div>
-@endsection
 
+@endsection
