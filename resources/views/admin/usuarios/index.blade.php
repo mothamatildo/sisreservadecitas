9337e9a73a668a62bab6a3deaede6fd47c502a8c
mothamatildo <mothamatildo@gmail.com>
2026-03-03 05:39:53 -0500

    Configuración de DataTables en Español - Video 20

--- resources/views/admin/usuarios/index.blade.php
+++ resources/views/admin/usuarios/index.blade.php
@@ -40,8 +40,12 @@
     <td>{{$contador++}}</td>
     <td>{{$usuario->name}}</td>   
     <td>{{$usuario->email}}</td> 
-    <td>
-      ver / editar / borrar
+    <td style="text-align: center">
+    <div class="btn-group" role="group" aria-label="Basic example">
+  <button type="button" class="btn btn-info btn-sm"><i class="bi bi-eye"></i></button>
+  <button type="button" class="btn btn-success btn-sm"><i class="bi bi-pencil"></i></button>
+  <button type="button" class="btn btn-danger btn-sm"><i class="bi bi-trash"></i></button>
+</div>
     </td>
 </tr> 
 @endforeach  
@@ -50,22 +54,57 @@
 </table>
 
 <script>
-  $(function () {
-    $("#example1").DataTable({
-      "responsive": true, "lengthChange": false, "autoWidth": false,
-      "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
-    }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
-    $('#example2').DataTable({
-      "paging": true,
-      "lengthChange": false,
-      "searching": false,
-      "ordering": true,
-      "info": true,
-      "autoWidth": false,
-      "responsive": true,
-    });
-  });
-</script>
+                        $(function () {
+                            $("#example1").DataTable({
+                                "pageLength": 10,
+                                "language": {
+                                    "emptyTable": "No hay información",
+                                    "info": "Mostrando _START_ a _END_ de _TOTAL_ Usuarios",
+                                    "infoEmpty": "Mostrando 0 a 0 de 0 Usuarios",
+                                    "infoFiltered": "(Filtrado de _MAX_ total Usuarios)",
+                                    "infoPostFix": "",
+                                    "thousands": ",",
+                                    "lengthMenu": "Mostrar _MENU_ Usuarios",
+                                    "loadingRecords": "Cargando...",
+                                    "processing": "Procesando...",
+                                    "search": "Buscador:",
+                                    "zeroRecords": "Sin resultados encontrados",
+                                    "paginate": {
+                                        "first": "Primero",
+                                        "last": "Ultimo",
+                                        "next": "Siguiente",
+                                        "previous": "Anterior"
+                                    }
+                                },
+                                "responsive": true, "lengthChange": true, "autoWidth": false,
+                                buttons: [{
+                                    extend: 'collection',
+                                    text: 'Reportes',
+                                    orientation: 'landscape',
+                                    buttons: [{
+                                        text: 'Copiar',
+                                        extend: 'copy',
+                                    }, {
+                                        extend: 'pdf'
+                                    },{
+                                        extend: 'csv'
+                                    },{
+                                        extend: 'excel'
+                                    },{
+                                        text: 'Imprimir',
+                                        extend: 'print'
+                                    }
+                                    ]
+                                },
+                                    {
+                                        extend: 'colvis',
+                                        text: 'Visor de columnas',
+                                        collectionLayout: 'fixed three-column'
+                                    }
+                                ],
+                            }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+                        });
+                    </script>
               
               </div>
 
